bbb04f6cf28b2af41b38a4e343ab44c43946e1bd
lignas <alwaysone312@gmail.com>
2025-09-14 18:51:39 +0900

    refactor: WebClient 빈 등록으로 변경 및 외부 결제 API 메서드 테스트 코드 추가

--- src/main/java/com/allra/market/domain/product/infrastructure/PaymentApiService.java
+++ src/main/java/com/allra/market/domain/product/infrastructure/PaymentApiService.java
@@ -4,14 +4,11 @@ import com.allra.market.common.exception.dto.ExternalApiException;
 import com.allra.market.domain.payment.application.response.PaymentResultDto;
 import com.allra.market.domain.product.infrastructure.request.PaymentRequest;
 import com.allra.market.domain.product.infrastructure.response.PaymentResponse;
-import io.netty.channel.ChannelOption;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.http.client.HttpClient;
 
-import java.time.Duration;
 import java.util.Objects;
 
 import static com.allra.market.common.exception.enums.ErrorCode.PAYMENT_FAILED;
@@ -19,13 +16,16 @@ import static com.allra.market.common.exception.enums.ErrorCode.PAYMENT_FAILED;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class PaymentApiService {
+
+    private final WebClient webClient;
 
     public PaymentResultDto externalPaymentApiCall(Long orderId, Long totalPrice) {
         PaymentRequest request = new PaymentRequest(orderId.toString(), totalPrice);
 
         try {
-            PaymentResponse response = webClient().post()
+            PaymentResponse response = webClient.post()
                     .uri("https://allramarket.free.beeceptor.com/api/v1/payment")
                     .bodyValue(request)
                     .retrieve()
@@ -40,15 +40,5 @@ public class PaymentApiService {
             log.error("외부 결제 API 호출 실패 - orderId: {}", orderId, e);
             throw new ExternalApiException(PAYMENT_FAILED);
         }
-    }
-
-    private WebClient webClient() {
-        HttpClient httpClient = HttpClient.create()
-                .responseTimeout(Duration.ofSeconds(3))
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 3000);
-
-        return WebClient.builder()
-                .clientConnector(new ReactorClientHttpConnector(httpClient))
-                .build();
     }
 }
